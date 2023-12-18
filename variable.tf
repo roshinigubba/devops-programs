892019c609c51577bf994739cd3afb3648133429
roshinigubba <153551746+roshinigubba@users.noreply.github.com>
2023-12-19 05:13:18 +0530

    Update RG_Variables-tf-18-12-2023

--- variable.tf
+++ variable.tf
@@ -1,3 +1,14 @@
+#Main.tf
+#resource block:
+
+resource "local_file" "hi-pet"{
+filename=var.filename
+content=var.content
+}
+-------------------------------------------
+#variable.tf
+#variable block
+
 variable "filename"{
 default="/root/hipet.txt"
 
